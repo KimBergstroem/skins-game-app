cf230607e0c54fb04f9b452dd0f72d1820904a85
KimBergstroem <kimmen_b@hotmail.com>
2024-06-10 15:53:46 +0200

    style: add log in button, styled page with diff colors

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,21 @@
 <template>
   <div class="container">
     <header>
-      <h1>Game</h1>
+      <h1>Simple Easy Skins</h1>
       <input v-model.trim="search" type="text" placeholder="Search..." />
+      <button class="btn-steam">Sign In With Steam</button>
     </header>
-    <div class="options-container">
-      <div v-for="quiz in quizes" :key="quiz.id" class="card">
-        <img :src="quiz.img" :alt="quiz.name" />
-        <div class="card-text">
-          <h2>{{ quiz.name }}</h2>
-          <p>{{ quiz.questions.length }} questions</p>
+    <div class="wrapper">
+      <h1>Games</h1>
+      <div class="options-container">
+        <div v-for="quiz in quizes" :key="quiz.id" class="card">
+          <img :src="quiz.img" :alt="quiz.name" />
+          <div class="card-text">
+            <h2>{{ quiz.name }}</h2>
+            <p>
+              <span>{{ quiz.questions.length }}</span> Online
+            </p>
+          </div>
         </div>
       </div>
     </div>
@@ -48,6 +54,7 @@ header {
 header h1 {
   font-weight: bold;
   margin-right: 30px;
+  color: white;
 }
 
 header input {
@@ -60,9 +67,24 @@ header input {
 .options-container {
   display: flex;
   flex-wrap: wrap;
-  margin-top: 40px;
+  margin-top: 15px;
 }
 
+.btn-steam {
+  background-color: rgb(30, 47, 78);
+  color: white;
+  border: none;
+  border-radius: 5px;
+  margin-left: 20px;
+  padding: 10px;
+  font-weight: bold;
+  cursor: pointer;
+}
+
+.wrapper {
+  margin-top: 50px;
+  color: white;
+}
 /* ------------------> CARD STYLE */
 .card {
   width: 310px;
@@ -72,6 +94,8 @@ header input {
   margin-bottom: 35px;
   margin-right: 20px;
   cursor: pointer;
+  background-color: rgb(30, 47, 78);
+  color: white;
 }
 
 .card img {
@@ -81,10 +105,15 @@ header input {
 }
 
 .card .card-text {
-  padding: 0 5px;
+  padding: 10px 10px;
 }
 
 .card .card-text h2 {
+  font-weight: bold;
+}
+
+.card .card-text span {
+  color: rgb(6, 255, 6);
   font-weight: bold;
 }
 </style>
